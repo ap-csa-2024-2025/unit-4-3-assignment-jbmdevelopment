51a24e26cb8a73f84478ece7068ac3c7f97b5f1e
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2024-11-07 11:06:42 -0800

    aaaaaaaaaaa

--- Main.java
+++ Main.java
@@ -4,6 +4,32 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    // #1 PRINT EVERY ODD NUMBER 1 - 25
+    for(int i = 0; i <= 25; i++) {
+         if(i % 2 == 1 ) { // check if it's odd
+          System.out.println(i);
+       }
+    }
+    // #2 
+    for(int i = 17; i <= 73; i++) {
+        System.out.print(i + " "); // add a space
+        if(i % 10 == 6) { // check if last digit is 6, every 10 numbers theend is 6.
+          System.out.println(" ");
+        }
+    }
+    // #3
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter a number 0 - 50");
+    int x = input.nextInt();
+    if(x < 0 || x > 50) {
+      System.out.println("erorr");
+    } else {
+      for(int i = 0; i <= 50; i++) {
+          System.out.print(i + " ");
+          if(i % 5 == 0) {
+            System.out.println(""); // line break
+          }
+      }
+    }
   }
 }
